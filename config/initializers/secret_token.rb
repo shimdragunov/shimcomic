2d7194fdcb8fd19f7db7802034b2c37875d71228
shimdragunov <lukerenia@gmail.com>
2012-02-17 14:19:01 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shimcomic::Application.config.secret_token = '8dd1da24bdb92a061fabd8a4056527e3729a52dbf680c5b83cbe9a33daa67b8358d231da25a6a691b758e9fa14685c0edbc48262e515a292c8f18cedaf26b5f2'
